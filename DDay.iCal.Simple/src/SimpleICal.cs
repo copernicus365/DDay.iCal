217e6da485f8a13aa6869d6d6077ca4e4eac8b70
copernicus365 <copernicus365@gmail.com>
2013-12-10 18:03:02 -0500

    Minor changes, and deleted some excluded files from the project.

--- DDay.iCal.Simple/src/SimpleICal.cs
+++ DDay.iCal.Simple/src/SimpleICal.cs
@@ -13,6 +13,7 @@ namespace DDay.iCal.Simple
 		#region FIELDS / PROPERTIES
 
 		public DateTime EarliestIncludeEventTime { get; set; }
+		public DateTime LatestIncludeEventTime { get; set; }
 
 		public TimeZoneInfo DefaultTimeZone { get; set; }
 		public bool SetUpdatedEventTimeToStartTime = false;
@@ -31,6 +32,7 @@ namespace DDay.iCal.Simple
 		public SimpleICal(DateTime? earliestIncludeEventTime = null)
 		{
 			EarliestIncludeEventTime = earliestIncludeEventTime ?? DateTime.MinValue;
+			LatestIncludeEventTime = DateTime.MaxValue;
 		}
 
 		#endregion
@@ -66,7 +68,7 @@ namespace DDay.iCal.Simple
 		{
 			var calCollection = calendarCollection.ToArrayOrNull();
 			if (calCollection.IsNullOrEmpty())
-				return null;
+				return new SEvent[0];
 
 			_now = DateTime.UtcNow;
 
@@ -85,16 +87,17 @@ namespace DDay.iCal.Simple
 				foreach (var eventOrGroup in events) {
 
 					// if more than 1 (is a group), we'll REQUIRE it to be a recurrant event. if not is discarded
-					SEvent[] recurringEvents = GetRecurringEvents(eventOrGroup);					
-					if (!recurringEvents.IsNullOrEmpty())
-						fevents.AddRange(recurringEvents);
+					SEvent[] recurringEvents = GetRecurringEvents(eventOrGroup);
+					if (recurringEvents != null) {
+						if(recurringEvents.Length > 0) // if zero, means WAS a recurring event, but no occurrences found in range so continue
+							fevents.AddRange(recurringEvents);
+					}
 					else {
 						var evnt = eventOrGroup.First;
-						
 						if (evnt.Start == null) continue;
 						if (evnt.End == null) evnt.End = evnt.Start;
 
-						if (evnt.End.Ticks >= EarliestIncludeEventTime.Ticks) {
+						if (evnt.End.Value >= EarliestIncludeEventTime && evnt.Start.Value < LatestIncludeEventTime) {
 							var ev = ToEvent(evnt);
 							if (ev != null)
 								fevents.Add(ev);
@@ -104,18 +107,9 @@ namespace DDay.iCal.Simple
 			}
 			return fevents
 				.Where(e => e != null)
-				//.Distinct( // not needed anymore, our use of GroupByQuick already sorts ids
-				//	e => e.Id,
-				//	(e, d) => {
-				//		if (e.Id == null)
-				//			return null;
-				//		e.Id += "_" + e.Start.DateTime.XmlTime();
-				//		if (d.ContainsKey(e.Id))
-				//			return null;
-				//		return e;
-				//	})
 				.OrderBy(e => e.Start)
 				.ToArray();
+				//.Distinct(e => e.Id, (e, d) => { if (e.Id == null) return null; e.Id += "_" + e.Start.DateTime.XmlTime(); if (d.ContainsKey(e.Id)) return null; return e; })
 		}
 
 		/// <summary>
@@ -134,28 +128,41 @@ namespace DDay.iCal.Simple
 				return null;
 		
 			IRecurrencePattern recurPtrn = recurEvent.RecurrenceRules.First();
-			SEvent[] results = GetRecurringEvents(recurEvent, recurPtrn).Where(e => e != null).ToArray();
-			if (results == null)
+			SEvent[] recurrences = GetRecurringEvents(recurEvent, recurPtrn).Where(e => e != null).ToArray();
+			if (recurrences == null)
 				return con_EmptySEventsArr; // NULL return means was not recurring, so  we must return empty array
 			if (!group.HasMany)
-				return results;
+				return recurrences;
 
-				//e.RecurrenceRules.IsNullOrEmpty() &&  && 
 			DateTime maxRecurDT = _now.AddMonths(3);
-			SEvent[] others = group.Items
-				.Where(e => e.RecurrenceID != null && e.Start != null && e.Start.Value >= EarliestIncludeEventTime && e.Start.Value < maxRecurDT)
+			
+			// If here it means group.Items > 1, which means there are more than one VEVENT with the same ID
+			// the only way that is valid is if the extras are Recurrence alterations (e.RecurrenceID != null)
+			// The point of alteredEvents is to get any valid events with a recurrenceId.
+			// Note! We cannot filter these Items by (e.Start.Value >= EarliestIncludeEventTime && e.Start.Value < maxRecurDT)
+			// as we initially did! Reason: an event can be altered with a much later or earlier time, which 
+			// disqualifies it, but if so, we need to have it so we can simply delete that event from the final result below.
+			// confusing? sorry, read the code below
+
+			SEvent[] alteredEvents = group.Items
+				.Where(e => e.RecurrenceID != null && e.Start != null) // can't do following! && e.Start.Value >= EarliestIncludeEventTime && e.Start.Value < maxRecurDT)
 				.Select(e => ToEvent(e)).ToArray();
 
-			for (int i = 0; i < results.Length; i++) {
-				var r = results[i];
-				var o = others.FirstOrDefault(ev => ev.RecurrenceId == r.Start);
-				if (o != null) {
-					o.Id = r.Id;
-					results[i] = o;
+			bool hadOutOfRangers = false;
+			for (int i = 0; i < recurrences.Length; i++) {
+				var r = recurrences[i];
+				var e = alteredEvents.FirstOrDefault(ev => ev.RecurrenceId == r.Start);
+				if (e != null) {
+					e.Id = r.Id;
+					if (e.End < e.Start) e.End = e.Start;
+					bool outOfRange = e.End < EarliestIncludeEventTime || e.Start > maxRecurDT;
+					if (outOfRange) hadOutOfRangers = true;
+					recurrences[i] = outOfRange ? null : e;
 				}
 			}
-			
-			return results;
+			if (hadOutOfRangers)
+				recurrences = recurrences.Where(e => e != null).ToArray();
+			return recurrences;
 		}
 
 		public IEnumerable<SEvent> GetRecurringEvents(IEvent evnt, IRecurrencePattern recur)
@@ -170,33 +177,41 @@ namespace DDay.iCal.Simple
 			if (rEvent.RRuleStr.IsNullOrEmpty())
 				yield break;
 
-			DateTime defMaxDT = _now.AddMonths(1);
+			DateTime maxDT = _now.AddMonths(3);
 
-			IList<Occurrence> occurrences = evnt.GetOccurrences(EarliestIncludeEventTime, _now.AddMonths(1));
+			IList<Occurrence> occurrences = evnt.GetOccurrences(EarliestIncludeEventTime, maxDT);
 
 			if (occurrences == null)
 				yield break;
 
-			if (occurrences.Count < 2) {
-				occurrences = evnt.GetOccurrences(EarliestIncludeEventTime, _now.AddMonths(3));
-				if (occurrences == null || occurrences.Count == 0)
-					yield break;
-			}
+			//if (occurrences.Count < 2) {
+			//	occurrences = evnt.GetOccurrences(EarliestIncludeEventTime, maxDT);
+			//	if (occurrences == null || occurrences.Count == 0)
+			//		yield break;
+			//}
+			if (occurrences.IsNullOrEmpty())
+				yield break;
 
-			if (occurrences.Count > 6)
-				occurrences = occurrences.Take(6).ToList();
+			if (occurrences.Count > 9)
+				occurrences = occurrences.Take(9).ToList();
 
 			DateTime updated = evnt.LastModified.ToDateTime(evnt.Start.ToDateTime(_now));
 
 			for (int i = 0; i < occurrences.Count; i++) {
-
-				string idAppend = i < 1 ? null : "_recur-" + i;
-				var ev = rEvent.Copy(idAppend);
-
+	
 				IPeriod period = occurrences[i].Period;
+				if (period == null || period.StartTime == null)
+					continue;
+				if (period.EndTime == null)
+					period.EndTime = period.StartTime;
 
 				bool hasTime;
-				ev.Start = GetLocalTime(period.StartTime, _currentTZ, out hasTime);
+				DateTimeOffset start = GetLocalTime(period.StartTime, _currentTZ, out hasTime);
+				
+				string idAppend = i < 1 ? null : "_recur-" + i + "-" + start.Date.XmlTime();
+				var ev = rEvent.Copy(idAppend);
+
+				ev.Start = start;
 				ev.End = GetLocalTime(period.EndTime, _currentTZ, out hasTime);
 				ev.Updated = SetUpdatedEventTimeToStartTime ? ev.Start.DateTime : updated;
 				ev.IsAllDay = ev.Start.TimeOfDay == con_EmptyTime && ev.End.TimeOfDay == con_EmptyTime;
@@ -207,30 +222,14 @@ namespace DDay.iCal.Simple
 
 		#endregion
 
-		//public List<GroupKV<IEvent,string>> GetEventsTEMP(IICalendar[] calCollection)
-		//{
-		//	if (calCollection == null || calCollection.Length < 1)
-		//		return null;
-
-		//	__EarliestIncludeEventTime = EarliestDateForExcludingEvents();
-		//	__Now = DateTime.UtcNow;
-
-		//	var events = new List<SEvent>();
-
-		//	var cal = calCollection.First();
-
-		//	var list = cal.Events.GroupByQuick(e => e.UID);
-
-		//	return list;
-		//}
-
 		public SEvent ToEvent(IEvent evnt, bool isRecurrenceRuleOnly = false)
 		{
 			if (evnt == null || evnt.UID.IsNullOrEmpty())
 				return null;
 
 			if (evnt.Start == null) evnt.Start = new iCalDateTime(_now);
-			if (evnt.End == null) evnt.End = new iCalDateTime(_now);
+			if (evnt.End == null) evnt.End = evnt.Start;
+
 			if (evnt.LastModified == null) evnt.LastModified = new iCalDateTime(_now);
 
 			var cEvent = new SEvent();
